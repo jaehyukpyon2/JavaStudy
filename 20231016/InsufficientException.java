c7966cdd1e4275c3902d7fcd5abe0531445a8921
jaehyukpyon <ipadforjay@gmail.com>
2023-10-17 16:41:49 +0900

    exception and regular expression

---
+++ 20231016/InsufficientException.java
@@ -0,0 +1,10 @@
+public class InsufficientException extends Exception {
+
+    public InsufficientException() {
+        ;
+    }
+
+    public InsufficientException(String msg) {
+        super(msg);
+    }
+}
